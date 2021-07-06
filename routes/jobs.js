c93749310901564ba5c8770d4bdaa64c598cac9a
Gareth Nelson <gareth.nelson@kainos.com>
2021-07-06 15:51:02 +0100

    FrontEnd comm with backend correctly

--- routes/jobs.js
+++ routes/jobs.js
@@ -3,14 +3,14 @@ const router = express.Router();
 const axios = require('axios').default;
 const backEndURL = process.env.BACK_END_URL;
 
-// Example
-router.get('/Hello', async function (req, res) {
-    try {
-        const response = await axios(`${backEndURL}/Hello`);
-        res.send(response.data);
-        res.status(200);
-      } catch (error) {
-        console.error(error);
-      }
-})
+router.get('/band/:substr', async function (req, res) {
+  try {
+      var path = `${backEndURL}/jobs/band/${req.params.substr}`;
+      const response = await axios(path);
+      res.send(response.data);
+      res.status(200);
+    } catch (error) {
+      console.error(error);
+    }
+});
 module.exports = router;
