f80238bbcf7a4abe084c687fa4d7855cac9c8ff2
Aileen Tracey <aileen.tracey@kainos.com>
2021-07-06 16:59:45 +0100

    add frontend njx and error messages

--- routes/capabilities.js
+++ routes/capabilities.js
@@ -3,14 +3,24 @@ const router = express.Router();
 const axios = require('axios').default;
 const backEndURL = process.env.BACK_END_URL;
 
-router.get('/findByJobId/:id', async function (req, res) {
-    try {
-        const id = req.params.id;
-        const response = await axios(`${backEndURL}/capabilities/findByJobId/${id}`);
-        res.send(response.data[0]);
-        res.status(200);
+
+router.get('/findByJobId', async function (req, res) {
+  try {
+      const jobID = req.query.jobID;
+        if(!isNaN(jobID)){
+          const response = await axios(`${backEndURL}/jobs/checkIfJobExists/${jobID}`);
+            if(response.data === true){
+              const response = await axios(`${backEndURL}/capabilities/findByJobId/${jobID}`);
+              res.render('viewCapabilityByJobIdResults',  { capability: response.data, jobID }); 
+              res.status(200);
+            } else {
+              res.render('viewCapabilityByJobIdForm',  { error: 'Employee ID does not exist. Select again'}); 
+          }
+        } else {
+          res.render('viewCapabilityByJobIdForm',  { error: 'Invalid selection. Enter Integer'}); 
+        }
       } catch (error) {
         console.error(error);
-      }
+    }
 })
 module.exports = router;
