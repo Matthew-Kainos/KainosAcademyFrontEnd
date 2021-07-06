e60113b6c28a2f935716a77912ab9e7628a641c3
Aileen Tracey <aileen.tracey@kainos.com>
2021-07-06 11:23:52 +0100

    Initilise frontend

---
+++ app.js
@@ -0,0 +1,21 @@
+const express = require('express');
+const app = express();
+require('dotenv').config();
+
+const jobs  = require('./routes/jobs');
+
+app.use(express.json());
+app.use(express.urlencoded());
+
+app.use('/jobs', jobs);
+
+// 404 Path
+app.use((req, res) => {
+    res.status(404).json({
+        message: `Unable to find path ${req.path}`
+    })
+})
+
+app.listen(3001, function() { 
+    console.log('Express FrontEnd started') 
+ });
---
+++ routes/jobs.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const router = express.Router();
+const axios = require('axios').default;
+const backEndURL = process.env.BACK_END_URL;
+
+// Example
+router.get('/Hello', async function (req, res) {
+    try {
+        const response = await axios(`${backEndURL}/Hello`);
+        res.send(response.data);
+        res.status(200);
+      } catch (error) {
+        console.error(error);
+      }
+})
+module.exports = router;
