abea7706f527ce91442ab5640db8bbb2ceb6f2dd
Aileen Tracey <aileen.tracey@kainos.com>
2021-07-08 15:49:08 +0100

    add selenium tests

--- routes/capabilities.js
+++ routes/capabilities.js
@@ -3,23 +3,28 @@ const router = express.Router();
 const axios = require('axios').default;
 const backEndURL = process.env.BACK_END_URL;
 
-
 router.get('/findByJobId', async function (req, res) {
   try {
       const jobID = req.query.jobID;
-        if(!isNaN(jobID)){
+        if(!isNaN(jobID) && jobID){
           const response = await axios(`${backEndURL}/jobs/checkIfJobExists/${jobID}`);
             if(response.data === true){
               const response = await axios(`${backEndURL}/capabilities/findByJobId/${jobID}`);
               res.render('viewCapabilityByJobIdResults',  { capability: response.data, jobID }); 
               res.status(200);
             } else {
-              res.render('viewCapabilityByJobIdForm',  { error: 'Employee ID does not exist. Select again'}); 
+              res.render('viewCapabilityByJobIdForm',  { error: 'Employee ID does not exist. Select again'});
+              res.status(400); 
           }
         } else {
-          res.render('viewCapabilityByJobIdForm',  { error: 'Invalid selection. Enter Integer'}); 
+          res.render('viewCapabilityByJobIdForm',  { error: 'Invalid selection. Enter Integer'});
+          res.status(400);
         }
       } catch (error) {
+        if(error.code === 'ECONNREFUSED'){
+          res.send('Backend not running');
+          res.status(500);
+        }
         console.error(error);
     }
 })
