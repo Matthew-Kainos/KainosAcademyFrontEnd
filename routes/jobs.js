7a6fa717e3d718150661d19ad49d645102aadea4
Aileen Tracey <aileen.tracey@kainos.com>
2021-07-13 16:29:59 +0100

    eslint fixes

--- routes/jobs.js
+++ routes/jobs.js
@@ -5,51 +5,51 @@ const axios = require('axios').default;
 
 const backEndURL = process.env.BACK_END_URL;
 
-router.get('/allJobIds', async function (req, res) {
-    try {
-        const response = await axios(`${backEndURL}/jobs/allJobIds`);
-        res.send(response.data);
-        res.status(200);
-      } catch (error) {
-        console.error(error);
-      }
-})
-
-router.get('/viewRoleByBand', async function (req, res) {
+router.get('/allJobIds', async (req, res) => {
   try {
-      var path = `${backEndURL}/jobs/band`;
-      console.log(path);
-      const response = await axios(path);
-      res.render("viewRoleByBand",{
-        roles: response.data,
-        error: "No Errors"
-      })
-      res.status(200);
-    } catch (error) {
-      console.error(error);
-    }
+    const response = await axios(`${backEndURL}/jobs/allJobIds`);
+    res.send(response.data);
+    res.status(200);
+  } catch (error) {
+    console.error(error);
+  }
+});
+
+router.get('/viewRoleByBand', async (req, res) => {
+  try {
+    const path = `${backEndURL}/jobs/band`;
+    console.log(path);
+    const response = await axios(path);
+    res.render('viewRoleByBand', {
+      roles: response.data,
+      error: 'No Errors',
+    });
+    res.status(200);
+  } catch (error) {
+    console.error(error);
+  }
 });
 
 router.post('/viewRoleByBand', async (req, res) => {
   try {
-    var path = `${backEndURL}/jobs/band/${req.body.roleInput.replace(' ', '_')}`;
+    const path = `${backEndURL}/jobs/band/${req.body.roleInput.replace(' ', '_')}`;
     const response = await axios(path);
-    if(typeof response.data == 'string'){
-      res.render("viewRoleByBand",{
+    if (typeof response.data === 'string') {
+      res.render('viewRoleByBand', {
         roles: [],
-        error: response.data
-      })
+        error: response.data,
+      });
     }
-    if(typeof response.data == 'object'){
-      res.render("viewRoleByBand",{
+    if (typeof response.data === 'object') {
+      res.render('viewRoleByBand', {
         roles: response.data,
-        error: "No Errors"
-      })
+        error: 'No Errors',
+      });
     }
   } catch (error) {
     console.error(error);
   }
-}); 
+});
 
 router.get('/getAllJobs', async (req, res) => {
   if (req.session.isLoggedIn) {
