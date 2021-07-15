042f813d8ca6994e4abb5f4d98a49a2cb607d724
Aileen Tracey <aileen.tracey@kainos.com>
2021-07-15 16:54:12 +0100

    add testing for add role function

--- tests/uiTests/addTests.js
+++ tests/uiTests/addTests.js
@@ -16,7 +16,7 @@ const adminDetails = {
   password: process.env.DB_Admin_Pw,
 };
 
-describe.only('Add', function () {
+describe('Add', function () {
   this.timeout(0);
   beforeEach(async () => {
     driver = await new webdriver.Builder().forBrowser('chrome').build();
@@ -33,7 +33,6 @@ describe.only('Add', function () {
   });
   describe('Role', () => {
     it('should successfully return popup message if trying to add role of invalid name length', async () => {
-      await driver.manage().window().maximize();
       await driver.get('http://localhost:3001/add/role');
       const title = await driver.getTitle();
       expect(title).equal('Admin Add Role');
@@ -52,12 +51,11 @@ describe.only('Add', function () {
       expect(errorText).to.equal('Invalid Role Name length');
     });
     it('should successfully return popup message if trying to add role of invalid summary length', async () => {
-      await driver.manage().window().maximize();
       await driver.get('http://localhost:3001/add/role');
       const title = await driver.getTitle();
       expect(title).equal('Admin Add Role');
       await driver.findElement(webdriver.By.id('RoleName')).sendKeys('Valid Name Length');
-      await driver.findElement(webdriver.By.id('SpecSum')).sendKeys('abc');
+      await driver.findElement(webdriver.By.id('SpecSum')).sendKeys('a');
       await driver.findElement(webdriver.By.id('SpecLink')).sendKeys('Fake Link');
       await driver.findElement(webdriver.By.css('#Capability > option:nth-child(1)'))
         .click();
@@ -69,6 +67,60 @@ describe.only('Add', function () {
       expect(titleAfterSubmission).equal('Admin Add Role');
       const errorText = await driver.findElement(webdriver.By.id('popupMessage')).getText();
       expect(errorText).to.equal('Invalid Summary length. Maximum 255 characters. Mimimum 10');
+    });
+    it('should successfully return popup message if trying to add role of invalid link length', async () => {
+      await driver.get('http://localhost:3001/add/role');
+      const title = await driver.getTitle();
+      expect(title).equal('Admin Add Role');
+      await driver.findElement(webdriver.By.id('RoleName')).sendKeys('Valid Name Length');
+      await driver.findElement(webdriver.By.id('SpecSum')).sendKeys('Valid Summary Length');
+      await driver.findElement(webdriver.By.id('SpecLink')).sendKeys('a');
+      await driver.findElement(webdriver.By.css('#Capability > option:nth-child(1)'))
+        .click();
+      await driver.findElement(webdriver.By.css('#Band > option:nth-child(2)'))
+        .click();
+      await driver.executeScript('window.scrollTo(0,10000);');
+      await driver.findElement(webdriver.By.id('AddSubmitButton')).click();
+      const titleAfterSubmission = await driver.getTitle();
+      expect(titleAfterSubmission).equal('Admin Add Role');
+      const errorText = await driver.findElement(webdriver.By.id('popupMessage')).getText();
+      expect(errorText).to.equal('Invalid Link length. Maximum 30 characters. Mimimum 5');
+    });
+    it.skip('should successfully return success popup message adding new role successful', async () => {
+      await driver.get('http://localhost:3001/add/role');
+      const title = await driver.getTitle();
+      expect(title).equal('Admin Add Role');
+      await driver.findElement(webdriver.By.id('RoleName')).sendKeys('Selenium Developer');
+      await driver.findElement(webdriver.By.id('SpecSum')).sendKeys('You make UI Tests');
+      await driver.findElement(webdriver.By.id('SpecLink')).sendKeys('This is a link');
+      await driver.findElement(webdriver.By.css('#Capability > option:nth-child(1)'))
+        .click();
+      await driver.findElement(webdriver.By.css('#Band > option:nth-child(2)'))
+        .click();
+      await driver.executeScript('window.scrollTo(0,10000);');
+      await driver.findElement(webdriver.By.id('AddSubmitButton')).click();
+      const titleAfterSubmission = await driver.getTitle();
+      expect(titleAfterSubmission).equal('Admin Add Role');
+      const errorText = await driver.findElement(webdriver.By.id('popupMessage')).getText();
+      expect(errorText).to.equal('New Role Selenium Developer Added');
+    });
+    it.skip('should successfully return success returm popup message if duplicate role name used', async () => {
+      await driver.get('http://localhost:3001/add/role');
+      const title = await driver.getTitle();
+      expect(title).equal('Admin Add Role');
+      await driver.findElement(webdriver.By.id('RoleName')).sendKeys('Selenium Developer');
+      await driver.findElement(webdriver.By.id('SpecSum')).sendKeys('You make UI Tests');
+      await driver.findElement(webdriver.By.id('SpecLink')).sendKeys('This is a link');
+      await driver.findElement(webdriver.By.css('#Capability > option:nth-child(1)'))
+        .click();
+      await driver.findElement(webdriver.By.css('#Band > option:nth-child(2)'))
+        .click();
+      await driver.executeScript('window.scrollTo(0,10000);');
+      await driver.findElement(webdriver.By.id('AddSubmitButton')).click();
+      const titleAfterSubmission = await driver.getTitle();
+      expect(titleAfterSubmission).equal('Admin Add Role');
+      const errorText = await driver.findElement(webdriver.By.id('popupMessage')).getText();
+      expect(errorText).to.equal('Unable to add Role due to Duplicate Role Name');
     });
   });
 });
